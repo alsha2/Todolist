0e0b6c71fe56aae32a104b152b657532875f6c2a
alsha2 <143660093+alsha2@users.noreply.github.com>
2023-09-02 20:25:13 -0400

    Update ToDoListForme.swift

--- ToDoListForme.swift
+++ ToDoListForme.swift
@@ -1,23 +1,32 @@
-//
-//  ToDoListForme.swift
-//  To do list
-//
-//  Created by Shahad Al-deewan on 2023-08-30.
-//
-
 import SwiftUI
-import Combine
 
 class ToDoListFormeVM: ObservableObject {
     @Published var name: String = ""
     @Published var completed: Bool = false
+    var id: String?
+    
+    var updating: Bool {
+        id = nil
+        return completed
+    }
+    
+    var isDisabled: Bool {
+        name.isEmpty
+    }
+    
+    init() {}
+    
+    init(_currentToDoList: ToDoList){
+        self.name = _currentToDoList.name
+        self.completed = _currentToDoList.completed
+    }
 }
 
 struct ToDoListForme: View {
     @EnvironmentObject var storeData: StoreData
     @ObservedObject var formVM: ToDoListFormeVM
     @Environment(\.presentationMode) var presentationMode
-    
+
     var body: some View {
         NavigationView {
             Form {
@@ -30,11 +39,11 @@ struct ToDoListForme: View {
             .navigationBarTitleDisplayMode(.inline)
         }
     }
-    
+
     func updateToDoList(){
         presentationMode.wrappedValue.dismiss()
     }
-    
+
     func addToDolist() {
         presentationMode.wrappedValue.dismiss()
     }
